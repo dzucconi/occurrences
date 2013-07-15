22a0613ffc508b64f1ef7c7c8e81001174551667
dzucconi <mail@damonzucconi.com>
2013-07-15 16:29:53 -0400

    Build links to xxith

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,8 +1,52 @@
-$.getJSON("/archiver/lib/data/data.json", function(data) {
-  var html = $.map(data, function(record) {
-    console.log(record);
-    return record.title;
-  });
+;(function(exports) {
+  var randomColor = function() {
+    return "#" + (Math.random() * 0xFFFFFF << 0).toString(16);
+  };
 
-  $("body").html(html.join("<br>"));
-});
+  var queryString = function(options) {
+    var _queryString = $.map(options, function(value, key) {
+      return key + "=" + value;
+    }).join("&");
+
+    return "http://xxith.com/?" + _queryString
+  }
+
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  var initialize = function() {
+    $.getJSON("/archiver/lib/data/data.json", function(data) {
+      var links = $.map(data, function(record) {
+        var date = record.mid.split("-");
+
+        var options = {
+          year: date[0],
+          month: months[date[1] - 1],
+          day: date[2],
+          color: randomColor(),
+          bgcolor: randomColor()
+        };
+
+        var href = queryString(options);
+
+        return "<a href='" + href + "' target='_blank'>" + record.title + "</a>";
+      });
+
+      $("body").html(links.join("<br>"));
+    });
+  }
+
+  $(function() { initialize(); });
+}(this));
