d122f0b1c0c17391db24b1c05af7941d87e5857b
dzucconi <mail@damonzucconi.com>
2013-07-15 18:25:37 -0400

    Add option for refreshing

--- archiver/lib/archiver.rb
+++ archiver/lib/archiver.rb
@@ -7,14 +7,18 @@ require "oj"
 
 require_relative "core_ext/hash"
 require_relative "core_ext/object"
+require_relative "core_ext/array"
 require_relative "core_ext/time"
 
 require_relative "archiver/scraper"
 require_relative "archiver/parser"
 require_relative "archiver/exporter"
 
-def archive!
-  parser    = Archiver::Parser.new(Archiver::Scraper.data)
+def archive!(refresh=false)
+  scraper = Archiver::Scraper.new({ refresh: refresh })
+  scraper.write!
+
+  parser    = Archiver::Parser.new(scraper.data)
   exporter  = Archiver::Exporter.new(parser.normalized)
 
   exporter.write!
