ac432e6d642fdeefb41edc459d495fd9ce8ae548
dzucconi <mail@damonzucconi.com>
2013-07-15 18:26:22 -0400

    Allow differnt filetypes to be written

--- archiver/lib/archiver/exporter.rb
+++ archiver/lib/archiver/exporter.rb
@@ -3,7 +3,7 @@ module Archiver
     attr_reader :data, :options
 
     def initialize(data, options={})
-      options.reverse_merge!({ filename: "data.json" })
+      options.reverse_merge!({ filename: "data", as: :json })
 
       @data     = data
       @options  = options
@@ -13,8 +13,14 @@ module Archiver
       Oj.dump(data, mode: :compat)
     end
 
+    def to_rb
+      data
+    end
+
     def write!
-      File.open("./lib/data/#{options[:filename]}", "w") { |f| f.write(to_json) }
+      File.open("./lib/data/#{options[:filename]}.#{options[:as]}", "w") do |f|
+        f.write(self.send("to_#{options[:as]}"))
+      end
     end
   end # Exporter
 end # Archiver
