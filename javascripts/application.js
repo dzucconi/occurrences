6b89faf2a77f56a242dcb4cc027593d7e6f4db5e
dzucconi <mail@damonzucconi.com>
2013-07-16 14:19:39 -0400

    Clean up

--- javascripts/application.js
+++ javascripts/application.js
@@ -22,7 +22,7 @@
   };
 
   $.randomColor = function() {
-    return "#" + ("000000" + (Math.random()*0xFFFFFF<<0).toString(16)).slice(-6);
+    return "#" + ("000000" + (Math.random() * 0xFFFFFF << 0).toString(16)).slice(-6);
   };
 }(jQuery));
 
