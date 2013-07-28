9547e66d401f0038b7335077125f0b3fb5b211d2
dzucconi <mail@damonzucconi.com>
2013-07-28 15:57:29 -0400

    Removes trailing comma

--- archiver/lib/archiver/defaults.rb
+++ archiver/lib/archiver/defaults.rb
@@ -5,7 +5,7 @@ module Archiver
       thursday:   { open: 11, close: 21 },
       friday:     { open: 11, close: 18 },
       saturday:   { open: 11, close: 18 },
-      sunday:     { open: 11, close: 18 },
+      sunday:     { open: 11, close: 18 }
     }
 
   end # Defaults
