b9f2aa863bb461a240de47a4a16a255e11a52bc5
dzucconi <mail@damonzucconi.com>
2013-07-28 15:33:46 -0400

    Include current operating hours in defaults module

---
+++ archiver/lib/archiver/defaults.rb
@@ -0,0 +1,12 @@
+module Archiver
+  module Defaults
+    OPERATING_HOURS = {
+      wednesday:  { open: 11, close: 18 },
+      thursday:   { open: 11, close: 21 },
+      friday:     { open: 11, close: 18 },
+      saturday:   { open: 11, close: 18 },
+      sunday:     { open: 11, close: 18 },
+    }
+
+  end # Defaults
+end # Archiver
